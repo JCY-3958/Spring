31372f8054beae833692bd2e874876ca93408437
JCY-3958 <w7e43958@naver.com>
2024-03-15 17:06:02 +0900

    2024-03-11~15 Java Spring

---
+++ ex02/src/main/java/com/codehows/domain/Criteria.java
@@ -0,0 +1,23 @@
+package com.codehows.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Criteria {
+	private int pageNum;
+	private int amount;
+	
+	//기본값 설정 1페이지 10개
+	public Criteria() {
+		this(1, 10);
+	}
+	
+	public Criteria(int pageNum, int amount) {
+		this.pageNum = pageNum;
+		this.amount = amount;
+	}
+}
